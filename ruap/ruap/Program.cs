461ca5e82f1f9b93d9e87950fa10591ca420165e
APuhanic <puhanicantonio@gmail.com>
2022-10-25 22:21:13 +0200

    Dodano ime i prezime

--- ruap/ruap/Program.cs
+++ ruap/ruap/Program.cs
@@ -7,6 +7,8 @@ namespace ruap
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Pozdrav iz aplikacije od Antonio Puhanić");
+
         }
     }
 }
